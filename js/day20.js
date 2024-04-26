fea5a02cfe3c546971972162d1d49d431f8621c9
Abdul Rehman <abdulrehmanarain713@gmail.com>
2024-04-26 20:25:04 +0500

    day 20 Complete

--- js/day20.js
+++ js/day20.js
@@ -50,6 +50,36 @@ setTimeout(() => {
 // ==> Set Interval
 // setInterval(function, Timeout);
 // Multiple time execute after timedelay that you give.
-setInterval( () =>{
-    console.log("Hello");
+let id = setInterval( () =>{
+    console.log("Hello World");
 },2000); 
+console.log(id);
+// TO stop the SetInterval 
+clearInterval(id);  // give variable of that setInterval that you want to stop.
+
+// ==> This keyword with Arrow Function.
+// Normal function depend on Scope, Normal function check who call
+// Arrow Function depend on Lexical Scope, Arrow function check who call my Parent. Arrow Function Scope depend on Parent Function Scope.
+// Arrow Function ka lye this hota ha uska parent ka this
+const students = {
+    name : "Abdul Rehman",
+    marks : 95,
+    prop: this, // global scope
+    getName : function () {   // Normal Function
+        return this.name;
+    },
+    getMarks : () =>{         // Arrow Function
+        console.log(this);    // Parents Scope => window
+        return this.marks;
+    },
+    getInfo1 : function (){
+        setTimeout(()=> {
+            console.log(this); // Student
+        }, 2000);
+    },
+    getInfo1 : function (){
+        setTimeout(function() {
+            console.log(this);  // Window
+        }, 2000);
+    }
+}; 
